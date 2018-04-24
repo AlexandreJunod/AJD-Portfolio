a9ab0a979b87dc2512f51a961af74759ffca111c
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2018-04-24 19:14:21 +0200

    Commit 3

---
+++ config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":1,"title":"","author":"JUNOD Alexandre","fileName":"CV_AJD.pdf","bounds":[[909,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
